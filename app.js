7a9cf736cf02d9e3824f0123c08c56c0dd55e6cc
westcorperations <onwusioneejikek04027@gmail.com>
2021-07-29 21:36:24 +0100

    product section finished about us section started

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 (function (window, document) {
 
 })(window, document);
+feather.replace()
--- vue.js
+++ vue.js
@@ -15,9 +15,16 @@ const App = {
       {image:'product1.jpg',title:'Our classic senator design',priceRange:'$50-$100'},
       {image:'product2.png',title:'classic Ankara designs',priceRange:'$50-$100'},
        {image:'product3.jpg',title:'classic international wears',priceRange:'$100-$300'},
-        {image:'product1.jpg',title:'Fashion Training for aspirants',priceRange:'Affordable'},
+        {image:'product6.jpg',title:'Fashion Training for aspirants',priceRange:'Affordable'},
         
-        ]
+        ],
+        
+       AboutUs:[
+       {image:'truck.svg',title:'Fast Delivery'},
+       {image:'dollar-sign.svg',title:'Very Affordable'},
+       
+         
+         ]
     }}, 
     
 methods: {
